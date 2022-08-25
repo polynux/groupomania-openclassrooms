5bcd23b3d3559deafc68e8f2ef7f658b56983f1b
Guillaume Dorce <polynux.dev@gmail.com>
2022-08-25 12:26:00 +0200

    get all posts

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import { Post } from '@prisma/client';
+import { getAllPosts } from '@/controller/PostController';
 
 const posts = Router();
 
-posts.get('/', (req, res) => {
-  res.send('Hello World!');
+posts.get('/', async (req, res) => {
+  try {
+    const postList: Post[] = await getAllPosts();
+    return res.status(200).send(postList);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 export default posts;
