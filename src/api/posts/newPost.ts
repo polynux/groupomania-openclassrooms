b3cdc9f51868b96c925832db024f5fd0fd0aab21
Guillaume Dorce <polynux.dev@gmail.com>
2022-09-02 10:18:08 +0200

    store file with uuid and store only name in db

--- src/api/posts/newPost.ts
+++ src/api/posts/newPost.ts
@@ -6,6 +6,7 @@ import { Request, Response } from 'express';
 export default async (req: Request, res: Response) => {
   try {
     req.body.authorId = req.userId;
+    req.body.image = req.file?.filename;
     const post: Post = Post.parse(req.body);
     const newPost: PrismaPost = await createPost(post);
 
--- src/controller/FileController.ts
+++ src/controller/FileController.ts
@@ -1,10 +1,11 @@
 import multer from 'multer';
 import path from 'path';
+import {v4 as uuidv4} from 'uuid';
 
 const storage = multer.diskStorage({
   destination: path.join(__dirname, '../../public/uploads'),
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, uuidv4() + path.extname(file.originalname));
   },
 });
 
