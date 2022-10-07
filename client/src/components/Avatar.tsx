ba682fc50d0e49acdc333984896ce99b0e99f191
Guillaume Dorce <polynux.dev@gmail.com>
2022-10-07 14:27:27 +0200

    fix avatar refresh 404 error

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -6,14 +6,22 @@ const Avatar = ({ user }: any) => {
   const gravatarUrl = gravatar.url(user.email, { s: '64', r: 'x', d: '404' }, true);
   const avatarUi = `https://ui-avatars.com/api/?name=${initials}&background=0D8ABC&color=fff&size=64`;
   const [avatar, setAvatar] = useState(avatarUi);
+  const [firstLoad, setFirstLoad] = useState(true);
 
-  fetch(gravatarUrl).then((response) => {
-    if (response.status === 200) {
-      setAvatar(gravatarUrl);
-    }
-  });
+  if (firstLoad) {
+    fetch(gravatarUrl).then((response) => {
+      if (response.status === 200) {
+        setAvatar(gravatarUrl);
+      }
+      setFirstLoad(false);
+    });
+  }
 
-  return <div className='avatar shrink-0'><img src={avatar} alt="avatar" className='rounded-full w-16 h-16 cursor-pointer' /></div>;
+  return (
+    <div className="avatar shrink-0">
+      <img src={avatar} alt="avatar" className="rounded-full w-16 h-16 cursor-pointer" />
+    </div>
+  );
 };
 
 export default Avatar;
